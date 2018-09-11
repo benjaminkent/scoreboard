fa98e253ce98a08143f39625bd9c9ac0a26ded67
Benjamin JEHL <benjaminjehl@Benjamins-MBP.localdomain>
2018-09-11 17:05:26 -0400

    add button updating

--- public/main.js
+++ public/main.js
@@ -1,3 +1,17 @@
-const main = () => {};
+let teamOneScore = 0;
+
+const addPoint = event => {
+  teamOneScore += 1;
+  let message = `${teamOneScore}`;
+  console.log(event.target);
+  event.target.textContent = message;
+};
+
+const main = () => {
+  const teamOneAdd = document.querySelector(".team-1-add");
+  teamOneAdd.addEventListener("click", addPoint);
+
+  const teamOneScore = document.querySelector(".team-1-score");
+};
 
 document.addEventListener("DOMContentLoaded", main);
