c16bf8d7d316728e798aa01efe4df8dca34589c5
Benjamin JEHL <benjaminjehl@Benjamins-MBP.fios-router.home>
2018-09-11 20:55:53 -0400

    add and subtract work, added some css

--- public/main.js
+++ public/main.js
@@ -1,17 +1,49 @@
-let teamOneScore = 0;
+let score = 0;
 
 const addPoint = event => {
-  teamOneScore += 1;
-  let message = `${teamOneScore}`;
-  console.log(event.target);
-  event.target.textContent = message;
+  console.log("does this need to be here??");
+  score += 1;
+  let message = `${score}`;
+  const teamOneScore = document.querySelector(".team-1-score");
+  teamOneScore.textContent = message;
+};
+
+const subtractPoint = event => {
+  console.log("does this need to be here??");
+  score -= 1;
+  let message = `${score}`;
+  const teamOneScore = document.querySelector(".team-1-score");
+  teamOneScore.textContent = message;
+};
+
+const teamTwoAddPoint = event => {
+  console.log("does this need to be here??");
+  score += 1;
+  let message = `${score}`;
+  const teamTwoScore = document.querySelector(".team-2-score");
+  teamTwoScore.textContent = message;
+};
+
+teamTwoSubtractPoint = () => {
+  console.log("does this need to be here??");
+  score -= 1;
+  let message = `${score}`;
+  const teamTwoScore = document.querySelector(".team-2-score");
+  teamTwoScore.textContent = message;
 };
 
 const main = () => {
   const teamOneAdd = document.querySelector(".team-1-add");
   teamOneAdd.addEventListener("click", addPoint);
 
-  const teamOneScore = document.querySelector(".team-1-score");
+  const teamOneSubtract = document.querySelector(".team-1-subtract");
+  teamOneSubtract.addEventListener("click", subtractPoint);
+
+  const teamTwoAdd = document.querySelector(".team-2-add");
+  teamTwoAdd.addEventListener("click", teamTwoAddPoint);
+
+  const teamTwoSubtract = document.querySelector(".team-2-subtract");
+  teamTwoSubtract.addEventListener("click", teamTwoSubtractPoint);
 };
 
 document.addEventListener("DOMContentLoaded", main);
